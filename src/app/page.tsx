c17cb70361bf8e839226fe3a56a16ba1a942d670
John Isutsah <johnisutsa@gmail,com>
2024-04-15 13:40:56 +0300

    add filter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import JobFilterSidebar from '@/components/JobFilterSidebar'
 import JobListItem from '@/components/JobListItem'
 import prisma from '@/lib/prisma'
 
@@ -13,12 +14,22 @@ export default async function Home() {
 
   return (
     <main className='max-w-5xl m-auto px-3 my-10 space-y-10'>
-      <h1>Jobs</h1>
-      <ul>
-        {jobs.map((job) => (
-          <JobListItem job={job} key={job.id} />
-        ))}
-      </ul>
+      <div className='space-y-5 text-center'>
+        <h1 className='text-4xl tracking-tight font-extrabold lg:text-5xl'>
+          Developer Jobs{' '}
+        </h1>
+        <p className='text-muted-foreground'>
+          Find your dream job.
+        </p>
+      </div>
+      <section className='flex flex-col md:flex-row gap-4'>
+        <JobFilterSidebar />
+        <div className='grow space-y-4'>
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
---
+++ src/lib/job-type.ts
@@ -0,0 +1,15 @@
+export const jobTypes = [
+    "Full-time",
+    "Part-time",
+    "Contract",
+    "Temporary",
+    "Internship",
+    "Volunteer",
+    "Remote",
+];
+
+export const locationTypes = [
+    "On-site",
+    "Remote",
+    "Hybrid",
+];
